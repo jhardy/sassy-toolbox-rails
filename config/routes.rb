cb73d3e807c03e4c29721df9df40cce0f3f0094f
Jared Hardy <jared@jaredhardy.com>
2013-10-14 07:48:00 -0700

    hook up some stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Sasstools::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'items#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
